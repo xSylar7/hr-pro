b955f90d2bc5dba81eaa44508b5edb1bf0f14fd4
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2019-12-24 19:14:15 +0300

    Update hr_pro.py

--- hr_pro.py
+++ hr_pro.py
@@ -1,5 +1,15 @@
 class Employee:
-	# Employee class here
+    def __init__(self, name, age, salary, employment_year):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.employment_year = employment_year
+
+    def get_working_years(self):
+        return date.today().year - int(self.employment_year)
+
+    def __str__(self):
+        return "Name: %s, Age: %s, Salary: %s, Working Years: %d" % (self.name, self.age, self.salary, self.get_working_years())
 
 class Manager(Employee):
     # Manager class here
