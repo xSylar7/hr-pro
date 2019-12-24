871b16fb390588597ad9a8979862a4e25a3040b2
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2019-12-24 19:26:41 +0300

    Update hr_pro.py

--- hr_pro.py
+++ hr_pro.py
@@ -1,21 +1,11 @@
 class Employee:
-    def __init__(self, name, age, salary, employment_year):
-        self.name = name
-        self.age = age
-        self.salary = salary
-        self.employment_year = employment_year
-
-    def get_working_years(self):
-        return date.today().year - int(self.employment_year)
-
-    def __str__(self):
-        return "Name: %s, Age: %s, Salary: %s, Working Years: %d" % (self.name, self.age, self.salary, self.get_working_years())
+   #Employee class here
 
 class Manager(Employee):
-    pass
+    #Manager class here
         
 def main():
-	pass
+	# main code here
 
 if __name__ == '__main__':
 	main()
